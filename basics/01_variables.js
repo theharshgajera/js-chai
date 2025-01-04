5c5000e33636f820035048a0a6b95a601e3fc119
Harsh Gajera <103769647+theharshgajera@users.noreply.github.com>
2025-01-04 07:40:53 +0000

    4th lec done

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144533
+let accountName = "Harsh"
+var accountPassword = "12345"
+accountCity =  "Ahmedabad"
+// accountId = 2
+accountName = "harry"
+let accountState;
+console.log(accountName);
+console.table([accountId, accountName, accountPassword, accountCity])
+
+
+// never use var as it is too buggish (issue in scope)
